00a328ebcdd01630747a407dfc37042a56d35820
FennecProg <118948382+FennecProg@users.noreply.github.com>
2023-08-23 07:25:27 +0100

    Update Convert_your_word_to_phrase.py
    Add correct code

--- Convert_your_word_to_phrase.py
+++ Convert_your_word_to_phrase.py
@@ -1,8 +1,58 @@
+from gtts import  gTTS  # pip install gtts
 
-name = input("Convert your word to phrase: ").upper()
+import pyttsx3 # pip install pyttsx3
+import os 
+import sys 
 
-list_verify =["SAMI","AHMED", "RAMI","ISLAM","AMINE"]
+def menu():
+    print("================================")
+    print("Convert a Text File into Speech") 
+    print("================================")
+    option= input("[A]-English\n[B]-French\n[C]-Arabic \n[M]-Menu\n[Q]-Quit\n======================\n[#]-Enter your Choice: ").upper()
 
-for x in list_verify:
-    if name == list_verify[0]:
+    if option == "M":
+        return menu() 
+    
+    #Exist 
+    if option == "Q": 
+        print("Thank you for using my App")
+    
+    #Input /Text , Name File
+    text=input("Enter your Text: ") 
+    name = input("name of your file: ")
+    
+    #voice = gTTS(text, lang='en', slow=True)
+    #voice='male_2-local')
+    
+    #English 
+    if option == "A":
+        tts= gTTS(text, lang='en', slow='True')
+    
+    #French
+    if option == "B": 
+        tts= gTTS(text, lang='fr', slow="True")
+        
+   #Arabic
+    if option=="C": 
+        tts= gTTS(text, lang='ar', slow="True") 
+    
+    # create file 
+    if not os.path.isfile(name):
+        f = open(name, "w")
+        f.write(name)
+        tts.save(name+'.'+'mp3')
+        print("Done!")
+        sys.exit()
+    #check if exist 
+    if os.path.exists(name):
+        name = input("It's Exist, Enter your new file: ")
+        f = open(name, "w")
+        f.write(name)
+        tts.save(name+'.'+'mp3')
+        print("Done!")
+        sys.exit()
+        
+      
+        
+menu()
         
